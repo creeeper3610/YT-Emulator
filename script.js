825160a297ee0b0388ac572f98d83b7f45098526
creeeper3610 <kmsmysk.sorato@gmail.com>
2026-05-05 20:09:12 +0900

    Change video fetching to use IDs instead of videos

--- script.js
+++ script.js
@@ -5,19 +5,16 @@ async function search() {
   if (!q) return;
 
   const res = await fetch(`/api/search?q=${encodeURIComponent(q)}`);
-  const videos = await res.json();
+  const ids = await res.json();
 
   const container = document.getElementById("results");
   container.innerHTML = "";
 
-  videos.forEach(v => {
+  ids.forEach(id => {
     const div = document.createElement("div");
     div.innerHTML = `
-      <h3>${v.title}</h3>
-      <img src="${v.thumbnail}" width="320">
-      <br>
       <iframe width="560" height="315"
-        src="https://www.youtube.com/embed/${v.id}"
+        src="https://www.youtube.com/embed/${id}"
         allowfullscreen>
       </iframe>
       <hr>
